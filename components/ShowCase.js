ea069651411f760abfa0719c3a79149ca3f43bf4
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-03-05 11:25:37 +0530

    some important changes

--- components/ShowCase.js
+++ components/ShowCase.js
@@ -2,9 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 // Images
-import one from "../public/assets/one.png";
-import two from "../public/assets/2.png";
-import three from "../public/assets/3.png";
+import AGI from "../public/assets/AGI.png";
 
 // Components
 import Layout from "./Layout";
@@ -16,26 +14,10 @@ export default function Showcase() {
     <Layout title={"Mohit Fun"}>
       <section className="bg-grid">
         <Header />
-        <main className="grid grid-cols-3 gap-3 p-5 justify-items-center">
+        <main className="h-auto flex flex-wrap gap-3 p-5 px-10 justify-left">
           <Link href="/AGoodIdea">
             <Image
-              src={one}
-              alt="A Good Idea"
-              width="350"
-              className="rounded-lg"
-            />
-          </Link>
-          <Link href="/AGoodIdea">
-            <Image
-              src={two}
-              alt="A Good Idea"
-              width="350"
-              className="rounded-lg"
-            />
-          </Link>
-          <Link href="/AGoodIdea">
-            <Image
-              src={three}
+              src={AGI}
               alt="A Good Idea"
               width="350"
               className="rounded-lg"
