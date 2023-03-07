93305ab2456cf6171a20bf7376fc3a47a19f1315
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-03-07 12:04:25 +0530

    ITMGOB Completed

--- components/Is_This_Movie_G_O_B/MovieContainer.js
+++ components/Is_This_Movie_G_O_B/MovieContainer.js
@@ -1,22 +1,29 @@
-import { useState } from 'react';
-import Image from 'next/image';
-import { FiSearch } from 'react-icons/fi';
-import API_KEY from '../../config';
-// import ReadSomeReviews from '../../pages/ReadSomeReviews';
-import Link from 'next/link';
+import { useState } from "react";
+import Image from "next/image";
+import { FiSearch } from "react-icons/fi";
+import API_KEY from "../../config";
 
 export default function MovieContainer() {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [searched, setSearched] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleOverview = () => {
+    setExpanded(!expanded);
+  };
 
   const handleSearch = async () => {
     try {
       setIsLoading(true);
-      const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`);
+      const res = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
+      );
       const data = await res.json();
-      const filteredData = data.results.filter(movie => movie.title.toLowerCase() === query.toLowerCase());
+      const filteredData = data.results.filter(
+        (movie) => movie.title.toLowerCase() === query.toLowerCase()
+      );
       setMovies(filteredData);
       setSearched(true);
     } catch (err) {
@@ -25,42 +32,91 @@ export default function MovieContainer() {
       setIsLoading(false);
     }
   };
-  
+
   const judgeMovie = (rating) => {
     if (rating < 4) {
-      return 'Bad';
+      return "Bad 😣";
     } else if (rating < 6) {
-      return 'Average';
+      return "Average 🙄";
     } else if (rating < 8) {
-      return 'Good';
+      return "Good 😃";
     } else {
-      return 'Amazing';
+      return "Amazing 😮";
     }
   };
 
   return (
-    <div id='movieContainer' className='w-full h-screen bg-slate-200 flex flex-col items-center px-10 pt-36'>
-      <h1 className='text-3xl lg:text-5xl italic text-center'>Is This Movie <span className='bg-green-700 px-2 pr-3 text-white'>Good</span> or <span className='bg-red-500 px-2 pr-3 text-white'>Bad</span>?</h1>
-      <div className='flex mt-10 w-full justify-center'>
-        <input type="text" placeholder='Search your movie here...' className='border bg-transparent border-gray-800 w-9/12 sm:w-4/12 outline-none px-3' value={query} onChange={(e) => setQuery(e.target.value)} />
-        <button onClick={handleSearch} className="border border-gray-800 p-3 px-5 hover:bg-green-700 hover:text-white">
-          <FiSearch/>
+    <div
+      id="movieContainer"
+      className="w-full h-auto bg-slate-100 flex flex-col items-center px-10 pt-28"
+    >
+      <h1 className="text-3xl lg:text-5xl italic text-center">
+        Is This Movie{" "}
+        <span className="bg-green-700 px-2 pr-3 text-white">Good</span> or{" "}
+        <span className="bg-red-500 px-2 pr-3 text-white">Bad</span>?
+      </h1>
+      <div className="flex mt-10 w-full justify-center">
+        <input
+          type="text"
+          placeholder="Search your movie here..."
+          className="border border-r-0 bg-transparent border-gray-800 bg-white w-9/12 sm:w-4/12 outline-none px-3 rounded-l-3xl"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          onClick={handleSearch}
+          className="border border-l-0 border-gray-800 p-3 px-5 bg-white hover:bg-green-700 hover:text-white rounded-r-3xl"
+        >
+          <FiSearch />
         </button>
       </div>
-      <main className='flex gap-8 p-8 flex-wrap w-full justify-center'>
+      <main className="flex gap-8 p-8 flex-wrap w-full justify-center">
         {isLoading && movies.length === 0 && <p>Loading...</p>}
-        {searched && query && movies.length === 0 && !isLoading && <p>No Movie Available with this title 😥. Try another one 😉.</p>}
+        {searched && query && movies.length === 0 && !isLoading && (
+          <p>No Movie Available with this title 😥. Try another one 😉.</p>
+        )}
 
         {movies.map((movie) => (
-          <div key={movie.id} className="flex h-auto bg-orange-300">
-            {movie.poster_path && <Image src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} width={150} height={200} />}
-            <div className='p-3 pt-10 relative'>
-              <h2 className='text-lg font-semibold'>{movie.title}</h2>
-              <p className='absolute top-0 right-0 bg-yellow-500 p-2'>{movie.vote_average}</p>
-              <p>{judgeMovie(movie.vote_average)}</p>
+          <div
+            key={movie.id}
+            className="flex h-auto bg-white p-5 rounded-lg shadow-lg shadow-slate-300 movie-card"
+          >
+            {movie.poster_path && (
+              <Image
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+                width={170}
+                height={200}
+                className="rounded-lg shadow-lg shadow-slate-800"
+              />
+            )}
+            <div className="p-3 pl-7 relative">
+              <h2 className="text-xl text-neutral-700 font-semibold">
+                {movie.title}
+              </h2>
+              <p className="absolute top-0 right-0 bg-yellow-500 rounded-3xl p-2 text-xs font-semibold text-white">
+                {movie.vote_average}
+              </p>
+              {/* <p className="text-xl font-semibold py-2">{judgeMovie(movie.vote_average)}</p> */}
+              {/* <p className="text-sm mt-1 movie-overview">{movie.overview}</p> */}
+              <p
+                className={`text-xl font-semibold py-2 ${judgeMovie(
+                  movie.vote_average
+                )}`}
+              >
+                {judgeMovie(movie.vote_average)}
+              </p>
+              <div
+                className={`movie-overview ${
+                  expanded ? "expanded" : ""
+                } text-xs`}
+              >
+                <p>{movie.overview}</p>
+              </div>
+              <button onClick={toggleOverview}>
+                ...{expanded ? "less" : "more"}
+              </button>
             </div>
-            {/* <Link href={`/IsThisMovieGoodOrBad/ReadSomeReviews?movieId=${movie.id}`}><p>Read Some Reviews</p></Link> */}
-
           </div>
         ))}
       </main>
--- pages/IsThisMovieGoodOrBad.js
+++ pages/IsThisMovieGoodOrBad.js
@@ -4,7 +4,7 @@ import Layout from "@/components/Layout";
 
 export default function IsThisMovieGoodOrBad() {
   return (
-    <Layout title={"Is This Movie Good Or Bad"} description={"Find amazing ideas for apps, websites, diy and many more."} keywords={"idea generator, idea tools, neal-fun, web-tools"} favicon={"/movie.ico"}>
+    <Layout title={"Is This Movie Good Or Bad"} description={"Find out if a movie is good or bad with this movie rating app. Search for your favorite movies and see their average rating and a brief overview."} keywords={"movie rating app, movie reviews, movie ratings, search movies, movie recommendations, movie suggestions, movie rating system, movie rating website, movie database, film ratings, neal.fun"} favicon={"/movie.ico"}>
       <main className="w-full h-screen flex flex-col bg-slate-100 text-slate-900">
       <AppHeader/>
       <MovieContainer/>
