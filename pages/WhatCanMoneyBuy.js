38aa0016cb62b15e6cca76934c7081a738429814
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-03-26 21:30:21 +0530

    updates

--- pages/WhatCanMoneyBuy.js
+++ pages/WhatCanMoneyBuy.js
@@ -1,8 +1,11 @@
 
+import Layout from "@/components/Layout";
 import MainPage from "@/components/WhatCanMoneyBuy/MainPage";
 
 export default function WhatCanMoneyBuy() {
   return (
+    <Layout title={"What Can Money Buy You?"} description={"Check out some amazing things that you can buy with money."} keywords={"Money facts, money game, neal fun money, What Can Money Buy You?"} favicon={"/wcmby.ico"}>
     <MainPage/>
+    </Layout>
   )
 }
