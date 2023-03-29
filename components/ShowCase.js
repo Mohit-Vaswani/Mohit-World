10cd158ebd347ccc393e2f591bcc4dafbf101a33
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-03-29 10:07:58 +0530

    Removing A good idea

--- components/ShowCase.js
+++ components/ShowCase.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 import Image from "next/image";
 
 // Images
-import AGI from "../public/assets/AGI.png";
 import ITMGOB from "../public/assets/ITMGOB.png";
 import Money from "../public/assets/Money.png"
 
@@ -30,14 +29,6 @@ export default function Showcase() {
             <Image
               src={ITMGOB}
               alt="Is This Movie Good Or Bad"
-              width="350"
-              className="rounded-lg"
-            />
-          </Link>
-          <Link href="/AGoodIdea" className="ease-in duration-300 hover:scale-105">
-            <Image
-              src={AGI}
-              alt="A Good Idea"
               width="350"
               className="rounded-lg"
             />
