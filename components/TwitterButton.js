7844120dcd59c6d80428b27f3213bc9e7e74f85a
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-04-01 17:13:42 +0530

    twitter button center

--- components/TwitterButton.js
+++ components/TwitterButton.js
@@ -8,9 +8,11 @@ export default function TwitterButton() {
     window.open(twitterUrl);
   }
   return (
+    <div className="flex justify-center items-center">
       <button onClick={handleButtonClick} className="flex gap-2 items-center border-2 border-white rounded p-2 text-center static sm:absolute bottom-0 right-0 m-5 hover:bg-blue-500">
         <BsTwitter />
         <p>Twitter</p>
       </button>
+    </div>
   );
 }
