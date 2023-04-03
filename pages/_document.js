90d1b4c72eb7eaebf0440e9949df7da65b339384
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-04-03 13:37:15 +0530

    interstitial ad network

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
         <Main />
         <NextScript />
         <script data-cfasync="false" type="text/javascript" data-adel="atag" src="//achcdn.com/script/atg.js" czid="qokvdtsjzv"></script>
+        <script data-cfasync="false" type="text/javascript" src="//geniusdexchange.com/a/display.php?r=6880406"></script>
       </body>
     </Html>
   )
