183dda82f0077a65e25f326b81434025e65fd657
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-04-15 08:16:22 +0530

    some app removed

--- components/ShowCase.js
+++ components/ShowCase.js
@@ -2,9 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 // Images
-import ITMGOB from "../public/assets/ITMGOB.png";
 import Money from "../public/assets/Money.png"
-import NHIE from "../public/assets/NHIE.png"
 
 // Components
 import Layout from "./Layout";
@@ -18,26 +16,10 @@ export default function Showcase() {
       <section>
         <Header />
         <main className="h-full w-full flex flex-wrap gap-8 p-5 px-10 justify-center">
-          <Link href="/Never-Have-I-Ever" className="ease-in duration-300 hover:scale-105">
-            <Image
-              src={NHIE}
-              alt="Never Have I Ever Game"
-              width="350"
-              className="rounded-lg"
-            />
-          </Link>
           <Link href="/WhatCanMoneyBuy" className="ease-in duration-300 hover:scale-105">
             <Image
               src={Money}
               alt="What Can Money Buy ?"
-              width="350"
-              className="rounded-lg"
-            />
-          </Link>
-          <Link href="/IsThisMovieGoodOrBad" className="ease-in duration-300 hover:scale-105">
-            <Image
-              src={ITMGOB}
-              alt="Is This Movie Good Or Bad"
               width="350"
               className="rounded-lg"
             />
---
+++ pages/Before-The-World-Ends.js
@@ -0,0 +1,11 @@
+import StarContainer from "@/components/BeforeTheWorldEnds/StarContainer"
+
+export default function BeforeTheWorldEnds() {
+  return (
+    <section className="w-full h-screen text-white bg-white" id="btwe">
+        <header>
+            <h1 className="pt-16 text-5xl text-center uppercase font-semibold font-mono">Before the w<span id="globe">🌍</span>rlds end</h1>
+        </header>
+    </section>
+  )
+}
