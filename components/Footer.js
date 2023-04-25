cf1d7e6d05c31229dc06acd7706af11719aa6b5b
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-04-25 19:44:11 +0530

    some changes

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,8 @@
 
 export default function Footer() {
   return (
-    <footer id="footer" className="flex flex-col text-base md:text-xl items-center h-4 sm:h-12 pt-28">
-      <p>Copyright &copy; Mohit.World 2023</p>
+    <footer id="footer" className="text-base md:text-xl h-4 sm:h-12 pt-72">
+      <p className="text-center">Copyright &copy; Mohit.World 2023</p>
     </footer>
   );
 }
