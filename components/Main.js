22717ae46b99d06afc4bdb03d66c172722db54b8
Mohit Vaswani <mohitvaswani010@gmail.com>
2023-04-25 19:35:05 +0530

    I will be back

--- components/Main.js
+++ components/Main.js
@@ -1,32 +1,32 @@
-import Link from "next/link";
-import Image from "next/image";
-import first from "../public/thumnail/btwe.jpg"
+// import Link from "next/link";
+// import Image from "next/image";
+// import first from "../public/thumnail/btwe.jpg"
 
-const Webdata = [
-    {
-        id: 1,
-        title: "Before the world ends",
-        image: first,
-        altdata: "Before the world ends",
-        link: "/Before-The-World-Ends"
-    },
-]
+// const Webdata = [
+//     {
+//         id: 1,
+//         title: "Before the world ends",
+//         image: first,
+//         altdata: "Before the world ends",
+//         link: "/Before-The-World-Ends"
+//     },
+// ]
 
-export default function Main() {
-    return (
-        <main className='w-full h-full flex flex-wrap px-10 justify-center my-6 gap-10'>
-            {
-                Webdata.map(data => {
-                    return (
-                        <Link href={data.link}>
-                            <article key={data.id} className="rounded-lg p-2">
-                                <Image src={data.image} alt={data.altdata} width={300} height={200} className="rounded-md image-shadow" />
-                                <h2 className="text-lg sm:text-2xl font-semibold mt-2">{data.title}</h2>
-                            </article>
-                        </Link>
-                    )
-                })
-            }
-        </main>
-    )
-}
+// export default function Main() {
+//     return (
+//         <main className='w-full h-full flex flex-wrap px-10 justify-center my-6 gap-10'>
+//             {
+//                 Webdata.map(data => {
+//                     return (
+//                         // <Link href={data.link}>
+//                         //     <article key={data.id} className="rounded-lg p-2">
+//                         //         <Image src={data.image} alt={data.altdata} width={300} height={200} className="rounded-md image-shadow" />
+//                         //         <h2 className="text-lg sm:text-2xl font-semibold mt-2">{data.title}</h2>
+//                         //     </article>
+//                         // </Link>
+//                     )
+//                 })
+//             }
+//         </main>
+//     )
+// }
