147f731ee14d891fda8614628028e141d31e8b20
Mohit <mohitvaswani010@gmail.com>
2023-10-01 22:51:18 +0530

    New portfolio setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,6 @@
-import Feed from "@components/Feed";
-import Footer from "@components/Footer";
-import Header from "@components/Header";
-
 const Home = () => (
   <section className='w-full flex-center flex-col'>
-    <Header/>
-    <Feed />
-    <Footer/>
+    <h1>This is my new portfolio from now.</h1>
   </section>
 );
 
--- components/Footer.js
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return (
-      <footer id="footer" className="text-base md:text-xl h-4 sm:h-12 pt-32">
-        <p className="text-center">Copyright &copy; Mohit.World 2023</p>
-      </footer>
-    );
-  }
-  
