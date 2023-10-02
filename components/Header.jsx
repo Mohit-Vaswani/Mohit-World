7799c120225260504b7e1b4194cdc2edaae0fef6
Mohit <mohitvaswani010@gmail.com>
2023-10-02 17:19:37 +0530

    some more updates

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,17 +1,35 @@
-const Header = () => {
-  return (
-    <nav className="flex justify-between items-center">
-        <div>
-            hii_mohit
-        </div>
-        <ul className="flex gap-6 items-center">
-            <li>about</li>
-            <li>work</li>
-            <li>resume</li>
-            <li>contact</li>
-        </ul>
-    </nav>
-  )
-}
+"use client"
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
-export default Header
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="flex justify-between items-center text-3xl my-40 mt-20 sm:mt-32">
+      <div className="logo font-semibold">hii_mohit</div>
+      <div className="mobile-nav-icon block md:hidden" onClick={toggleNav}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+      <ul
+        className={`${
+          isOpen ? 'block absolute p-10 mobileNav' : 'hidden'
+        } md:flex gap-12 items-center md:items-stretch mt-4 md:mt-0`}
+      >
+        <div className="mobile-nav-icon block md:hidden absolute top-0 right-0 m-10 sm:m-20" onClick={toggleNav}>
+        {isOpen ? <FaTimes /> : <FaBars />}
+      </div>
+        <li>about</li>
+        <li>work</li>
+        <li>resume</li>
+        <li>contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Header;
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,9 +3,9 @@ import Header from './Header'
 
 const Hero = () => {
   return (
-    <main className='flex flex-col justify-center w-full h-screen md:px-28'>
+    <main className='flex flex-col w-full h-screen px-10 md:px-32 lg:px-52'>
         <Header/>
-        <h1 className='text-7xl'>hello I'm <span className='Croissant'>mohit</span> <br /> - a developer <br /> w/ a focus on frontend <br /> <span className='Playfair'>&</span> scaling products</h1>
+        <h1 className='text-4xl sm:text-5xl lg:text-7xl'>hello I'm <span className='Croissant'>mohit</span> <br /> - a developer <br /> w/ a focus on frontend <br /> <span className='Playfair'>&</span> scaling products.</h1>
     </main>
   )
 }
